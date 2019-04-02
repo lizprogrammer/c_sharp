15a3a8a4266e30c9bb9c6db0e64660fa4916a0b7
Elizabeth Rosso <lizprogrammer@yahoo.com>
2019-04-01 23:36:44 -0400

    Hackarank

---
+++ Hackarank/Day13/day13.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+abstract class Book
+{
+    
+    protected String title;
+    protected  String author;
+    
+    public Book(String t,String a){
+        title=t;
+        author=a;
+    }
+    public abstract void display();
+
+
+}
+
+//Write MyBook class
+class MyBook : Book
+{
+    public MyBook(String t, String a, double p) : base(t, a)
+    { 
+        
+    } 
+    public override void display() // Generates CS0506.
+    {
+        // Do something else.
+    }
+}
+
+class Solution {
+    static void Main(String[] args) {
+      String title=Console.ReadLine();
+      String author=Console.ReadLine();
+      int price=Int32.Parse(Console.ReadLine());
+      Book new_novel=new MyBook(title,author,price);
+      new_novel.display();
+    }
+}
