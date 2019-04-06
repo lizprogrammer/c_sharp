e492e1d25186f1f19be5d9237e70e8a3079d0a93
Elizabeth Rosso <lizprogrammer@yahoo.com>
2019-04-06 11:14:42 -0400

    connect 4

---
+++ Connect4/Board.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Connect4
+{
+    class Board
+    {
+      const int height = 6;
+      const int width = 7;
+ 
+
+    public void applyMove()
+    {
+      Console.WriteLine("Hello World!");
+
+      Move setMove = new Move(0);
+
+
+
+    }
+
+   }
+
+}
---
+++ Connect4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Connect4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Board myBoard = new Board();
+
+            myBoard.applyMove();
+        }
+    }
+}
